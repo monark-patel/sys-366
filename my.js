4310dd8fc1470a62e45d9ff83029d6c9cd3ddba2
Mypatel2 <mypatel2@myseneca.ca>
2019-02-26 15:14:48 -0500

    uml projects and case study
    uml projects and case study

---
+++ my.js
@@ -0,0 +1,64 @@
+function getStakeholdersList(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="width:100%; height:100%;" type="text/html" data="work/Team/StakeholdersList.html" ></object>';
+}
+
+function getRentalUML(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/MonarkPatel/RentalAndEquipmentServicingBUC.png" ></object>';
+}
+
+function getStoreUML(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/PayalDonda/StoreManagementBUC.png" ></object>';
+}
+
+function getInventoryUML(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/SahibArora/MaintainingInventoryBUC.png" ></object>';
+}
+
+function CustOrder(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/PayalDonda/FullfillCustomerOrder.html" ></object>';
+}
+
+function PayRec(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/PayalDonda/PaymentRecieved.html" ></object>';
+}
+
+function ProfRep(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/PayalDonda/CreateProfitReport.html" ></object>';
+}
+
+function OrderTools(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/MonarkPatel/OrderTools.html" ></object>';
+}
+
+function RentTools(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/MonarkPatel/RentTools.html" ></object>';
+}
+
+function Workshops(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/MonarkPatel/Workshops.html" ></object>';
+}
+
+function OrderProd(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/SahibArora/OrderProducts.html" ></object>';
+}
+
+function StockTheShelves(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/SahibArora/StockTheShelves.html" ></object>';
+}
+
+function TransferInventoryFromWarehouse(){
+	document.getElementById("model").style.display	= 'block';
+	document.getElementById("getData").innerHTML = '<br>' + '<object style="border-radius:15px; width:100%; height:100%;"  data="work/SahibArora/TransferInventoryFromWarehouse.html" ></object>';
+}
